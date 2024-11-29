fbd0bdabaa8062b797997776358caaee7efbe148
Hasankhan170 <hasankhanali0316@gmail.com>
2024-11-29 21:10:20 +0500

    kuch tw krlia

--- index.js
+++ index.js
@@ -2,17 +2,53 @@ import express from 'express'
 import dotenv from "dotenv";
 import connectDB from './src/db/index.js'
 import cors from 'cors'
+import bcrypt from "bcrypt"
 dotenv.config()
 
 
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
 
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+  })
+
+  app.get("/encryptPassword", (req,res)=>{
+
+    const planePassword = process.env.PLANE_PASSWORD
+    bcrypt.hash(planePassword, 10, function(err, hash) {
+
+      if(err) return res.status(500).json({
+        message: 'Internal Server Error',
+      })
+        res.status(200).json({
+          message: 'Password Hashed Successfully',
+          hashedPassword: hash,
+        })
+        
+        
+    });
+
+
+  })
+
+  app.get("/comparePassword",(req,res)=>{
+    const planePassword = process.env.PLANE_PASSWORD
+    const hashedPassword =  "$2b$10$LJoUbGWc8zvFa8SEyIhJ1OILHYTTj0GduM/J/Ybd5hC/MBOzagF42"
+
+    bcrypt.compare(planePassword, hashedPassword, function(err, result) {
+
+      if(err) return res.status(500).json({
+        message: 'Internal Server Error',
+      })
+      
+      res.status(200).json({
+        isMatch: result,
+      })
+  });
   })
 
 
